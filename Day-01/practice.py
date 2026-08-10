9f869ca641e74dca31812afd4087b260e20edaef
Mohit707377 <mohit7073mj@gmail.com>
2026-08-10 12:28:03 +0530

    Create practice.py

---
+++ Day-01/practice.py
@@ -0,0 +1,28 @@
+# Day 01 - Python Basics
+
+# 1. Print Statement
+print("Hello, Data Analytics!")
+print("Starting my Python journey.")
+
+
+# 2. Comments
+# This is a single-line comment
+
+
+# 3. Variables
+name = "Mohit"
+age = 22
+cgpa = 6.5
+is_student = True
+
+print(name)
+print(age)
+print(cgpa)
+print(is_student)
+
+
+# 4. Data Types
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(is_student))
