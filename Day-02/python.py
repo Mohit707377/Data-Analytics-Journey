f1b154a1dabcd83eb92f3671428b6bf12b415f4a
Mohit Jangra <mohit7073mj@gmail.com>
2026-08-11 15:04:36 +0530

    Create python.py

---
+++ Day-02/python.py
@@ -0,0 +1,34 @@
+
+### 1. Write a program to take a person's name, age and address as input and display them
+name = input("Enter the your name :- ")
+age = int(input("Enter your age :- "))
+address = input("Enter your address:- ")
+
+print(name)
+print(age)
+print(address)
+
+
+
+### 2. Swap Two Variables
+
+num1 = int(input("Enter the first Number :- "))
+num2 = int(input("Enter the second Number :- "))
+
+temp=num1
+num1=num2
+num2=temp
+
+print("after Swap num1 =",num1," and num2= ",num2)
+
+ ### 3. Convert Float into Integer
+num1= float(input("Enter the number :- "))
+print(type(num1))
+
+num1 = int(num1)
+
+print(num1)
+print(type(num1))
+
+
+ 
